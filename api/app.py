7483bd29737e3e2953f422dfb27a381fa6dd558d
Per-Christian Berg <per-christian.berg@visma.com>
2019-07-08 16:19:08 +0200

    fiks

--- api/app.py
+++ api/app.py
@@ -1,13 +1,21 @@
-from flask import request, render_template 
+from flask import request, render_template, redirect, flash, url_for
 import flask
+from werkzeug.utils import secure_filename
+
 from keras.models import load_model as k_load_model
-from helpers.constants import Path
-from preprocessor.image_processor import get_decoded_base64_img, feature_extract_image, load_model, prepare_image, load_image
+from ai.src.helpers.constants import Path
+from ai.src.preprocessor.image_processor import get_decoded_base64_img, feature_extract_image, load_model, prepare_image, load_image
+
 import os
 import numpy as np
 
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "data/people")
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+print("Upload folder - - - - - " + UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 face_model = None 
 image_feature_model = None 
@@ -24,7 +32,18 @@ def load_models():
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template("camera.html")
+    return render_template("index.html")
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+    else:
+        posted_file = request.files['file']
+        filename = secure_filename(posted_file.filename)
+        posted_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -35,8 +54,6 @@ def predict():
     image = get_decoded_base64_img(base64_image)
     image = prepare_image(image, 299)
     feature_image = feature_extract_image(image, image_feature_model)
-    print(feature_image)
-    print(feature_image.shape)
     label = ["Per", "Liv"]
 
     prediction = face_model.predict(np.asarray([feature_image]))[0]
